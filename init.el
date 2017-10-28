2a21c5b6932806cf1760f8048ab149139197f34d
Dokyung Song <dokyung.song@gmail.com>
2017-10-28 12:42:26 -0700

    defun linux-c-mode

--- init.el
+++ init.el
@@ -33,9 +33,37 @@
 (add-to-list 'auto-mode-alist '("\\.text\\'" . markdown-mode))
 (setq markdown-command "pandoc -f markdown -t html -s --mathjax --highlight-style=pygments")
 
+(defun c-lineup-arglist-tabs-only (ignored)
+  "Line up argument lists by tabs, not spaces"
+  (let* ((anchor (c-langelem-pos c-syntactic-element))
+         (column (c-langelem-2nd-pos c-syntactic-element))
+         (offset (- (1+ column) anchor))
+         (steps (floor offset c-basic-offset)))
+    (* (max steps 1)
+       c-basic-offset)))
+
+(add-hook 'c-mode-common-hook
+          (lambda ()
+            ;; Add kernel style
+            (c-add-style
+             "linux-tabs-only"
+             '("linux" (c-offsets-alist
+                        (arglist-cont-nonempty
+                         c-lineup-gcc-asm-reg
+                         c-lineup-arglist-tabs-only))))))
+
+(defun linux-c-mode ()
+  (interactive)
+  (c-mode)
+  (setq indent-tabs-mode t)
+  (c-set-style "linux-tabs-only"))
+
+;(add-to-list 'auto-mode-alist '("\.c$" . linux-c-mode))
+
 (require 'neotree)
 (global-set-key (kbd "C-x 8") 'neotree-toggle)
 
+(setq-default show-trailing-whitespace t)
 (setq-default indent-tabs-mode nil)
 (setq tab-width 2)
 (setq column-number-mode t)
